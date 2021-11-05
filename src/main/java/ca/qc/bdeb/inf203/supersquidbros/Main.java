fa9f62917d43ee2c9c83e5f5265cbebd32dcba58
FlyingFish34 <redslimetuto@gmail.com>
2021-11-05 09:45:17 -0400

    Initial commit

---
+++ src/main/java/ca/qc/bdeb/inf203/supersquidbros/Main.java
@@ -0,0 +1,86 @@
+package ca.qc.bdeb.inf203.supersquidbros;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public static final double HEIGHT = 480;
+    public static final double WIDTH = 350;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Pane rootMenu = genererMenuBackground();
+        Image imageMenu = new Image("accueil.png");
+        ImageView imageViewMenu = new ImageView(imageMenu);
+        imageViewMenu.setLayoutY(50);
+        Button jouer = new Button("Jouer!");
+        jouer.setLayoutX(150);jouer.setLayoutY(350);
+        Button classement = new Button("Meilleurs scores");
+        classement.setLayoutX(123);classement.setLayoutY(400);
+        Button fermerLeJeu = new Button("Quitter");
+        fermerLeJeu.setLayoutY(450);fermerLeJeu.setLayoutX(147);
+        rootMenu.getChildren().addAll(imageViewMenu,jouer,classement,fermerLeJeu);
+
+        Pane rootClassement = genererMenuBackground();
+        Button retourMenu = new Button("Retour au menu");
+        retourMenu.setLayoutX(120);retourMenu.setLayoutY(450);
+        rootClassement.getChildren().add(retourMenu);
+
+        Pane rootPartie = genererMenuBackground();
+        Text text = new Text("UwU");
+        text.setFont(Font.font(70));
+        text.setLayoutX(120);text.setLayoutY(350);
+        rootPartie.getChildren().add(text);
+
+        Scene scenePartie = new Scene(rootPartie, WIDTH, HEIGHT);
+        Scene sceneClassement = new Scene(rootClassement, WIDTH, HEIGHT);
+        Scene sceneMenu = new Scene(rootMenu, WIDTH, HEIGHT);
+
+        fermerLeJeu.setOnAction((e) -> {
+            Platform.exit();
+        });
+
+        classement.setOnAction((e) -> {
+            stage.setScene(sceneClassement);
+        });
+
+        jouer.setOnAction((e) -> {
+            stage.setScene(scenePartie);
+        });
+
+        retourMenu.setOnAction((e) -> {
+            stage.setScene(sceneMenu);
+        });
+
+        stage.setTitle("Super Squid Bros");
+        stage.setScene(sceneMenu);
+        stage.show();
+    }
+
+    private Pane genererMenuBackground () {
+        Pane rootMenu = new Pane();
+        Canvas canvas = new Canvas(WIDTH, HEIGHT);
+        GraphicsContext contextMenu = canvas.getGraphicsContext2D();
+        Color bleu = Color.rgb(0, 0, 139);
+        contextMenu.setFill(bleu);
+        contextMenu.fillRect(0, 0, WIDTH, HEIGHT);
+        rootMenu.getChildren().add(canvas);
+        return rootMenu;
+    }
+}
